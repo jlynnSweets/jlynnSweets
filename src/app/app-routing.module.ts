f5cafa9c28f247720a946ce6f83a94b77a08abfe
Christopher Thomas <chris.thomas6@accesscfa.com>
2021-10-03 12:54:04 -0400

    deploy changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { OrderComponent } from './order/order.component';
+import { AboutComponent } from './about/about.component';
+const routes: Routes = [
+  {path: 'menu', component: MenuComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MenuComponent  } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule  } from './material.module';
+import { RouterModule } from '@angular/router'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContactComponent,
+    AboutComponent,
+    OrderComponent 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
