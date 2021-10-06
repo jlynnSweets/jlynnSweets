c05e15aab0d1c5d7546f280e03a171fbd112b899
Christopher Thomas <chris.thomas6@accesscfa.com>
2021-10-05 21:24:08 -0400

    DEPLOY

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   constructor() { }
-
+  panelOpenState = false;
   ngOnInit(): void {
+
   }
 
 }
